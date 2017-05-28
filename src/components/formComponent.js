a7b4965774b79b98cca8fafd76f057ff2c511b5f
guruganesh <guru96299@gmail.com>
2017-05-28 20:15:33 +0530

    initialized repo

---
+++ src/components/formComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class FormComponent extends React.Component {
+  render () {
+    return (
+      <div>Hello from Form !!</div>
+    )
+  }
+}
+export default FormComponent;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+// import './index.css';
+import Form from './components/formComponent';
+
+ReactDOM.render(<Form />, document.getElementById('root'));
+registerServiceWorker();
