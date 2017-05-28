71a4dd17c245fd1b8cc5cfa1730cfe64ddfa312f
guruganesh <guru96299@gmail.com>
2017-05-28 20:31:08 +0530

    BS configured

--- src/components/formComponent.js
+++ src/components/formComponent.js
@@ -3,7 +3,7 @@ import React from 'react';
 class FormComponent extends React.Component {
   render () {
     return (
-      <div>Hello from Form !!</div>
+      <div className="well text-center">React Redux Form Example</div>
     )
   }
 }
